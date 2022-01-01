5c6498830c25d1809d5e34594626958fc1881de5
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-01-01 09:18:17 +0530

    Time: 1160 ms (6.65%), Space: 10.3 MB (27.29%) - LeetHub

---
+++ burst-balloons/burst-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int len = 1; len <= n; ++len)
+            for (int left = 0; left < n - len; ++left) {
+                int right = left + len;
+                for (int k = left+1; k < right; ++k)
+                    dp[left][right] = max(dp[left][right], nums[left]*nums[k]*nums[right] + dp[left][k] + dp[k][right]);
+            }
+        return dp[0][n-1];
+    
+}
+};
+
