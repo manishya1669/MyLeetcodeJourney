c6ec363c1772e71ff70c689cfce4fe3ef1448714
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-01-20 16:59:54 +0530

    Added solution - LeetHub

---
+++ 19. Minimum Spanning Tree - GFG/19.-minimum-spanning-tree.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+   {
+       int parent[V];
+       int key[V];
+       bool mstset[V];
+       
+       for(int i=0;i<V;i++)
+           key[i] = INT_MAX,mstset[i] = false;
+       
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+       key[0] = 0;
+   
+       pq.push({key[0],0});
+       while(!pq.empty()){
+           int u = pq.top().second;
+           pq.pop();
+           
+           mstset[u] = true;
+           
+           for(auto it: adj[u]){
+               int v = it[0];
+               int weight = it[1];
+               
+               if(mstset[v]==false && weight < key[v]){
+                   
+                    key[v] =weight;
+                   pq.push({key[v],v});
+                  
+               }
+           }
+       }
+       int sum=0;
+       for(int i=0;i<V;i++){
+           sum+=key[i];
+       }
+       return sum;
+       
+   }
+
+
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
