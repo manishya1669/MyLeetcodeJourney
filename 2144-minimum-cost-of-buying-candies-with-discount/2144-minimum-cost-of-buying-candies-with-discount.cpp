78cf65318d83da73a5adfd6e929b2667e37d8fb3
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-01-22 21:42:15 +0530

    Time: 12 ms (12.50%), Space: 10.8 MB (75.00%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        
+       
+        int n = cost.size();
+       sort(cost.begin(),cost.end(),greater<int>());
+        int res = 0;
+        int count = 0;
+        for(int i =0;i<n;i++){
+            if(count ==2){
+                count =0;
+                continue;
+            }
+            res +=cost[i];
+            count++;
+        }
+        return res;
+        
+    }
+    
+};
+
+//  int d = n;
+//         for(int i = 0;i<n;i++){
+           
+//                 int sum = cost[i]+cost[i+1];
+//                 res+=sum;
+//                  i=i+3;
+               
