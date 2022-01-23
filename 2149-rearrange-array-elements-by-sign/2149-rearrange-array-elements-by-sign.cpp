4df47984082ac187231ecdcab9d48c771e5b32cf
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-01-23 13:21:50 +0530

    Time: 365 ms (7.69%), Space: 134.6 MB (7.69%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,18 +1,51 @@
 class Solution {
 public:
-   vector<int> rearrangeArray(vector<int>& nums) {
-		vector<int> res(nums.size(), 0);
-		int pi = 0, ni = 1;
-		for(int i = 0; i < nums.size(); i++){
-			if(nums[i] > 0){
-				res[pi] = nums[i];
-				pi += 2;
-			}
-			else{
-				res[ni] = nums[i];
-				ni += 2;
-			}
-		}
-		return res;
-	}
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        vector<int>pos;
+        vector<int>neg;
+        int n= nums.size();
+        for(int i = 0;i<n;i++){
+            if(nums[i]>0)pos.push_back(nums[i]);
+            else
+                neg.push_back(nums[i]);
+        }
+        int k =0;
+        
+        for(int i = 0;i<nums.size();i=i+2){
+            nums[i] =pos[k] ;
+            nums[i+1] = neg[k];
+            k++;
+        }
+        neg.clear();
+        pos.clear();
+        return nums;
+        
+       
+    }
 };
+// vector<int> neg;
+//         vector<int>pos;
+		
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]>=0)pos.push_back(nums[i]);
+//             else 
+//             neg.push_back(nums[i]); 
+            
+            
+//         }
+        
+//         int k=0 ;
+        
+//         for(int i=0;i<nums.size();i+=2)
+//         {
+//           if(k<pos.size())     {
+//             nums[i]=pos[k];
+//             nums[i+1]=neg[k];
+//           k++;}
+//         }
+        
+//         neg.clear();
+//         pos.clear();
+//         return nums;
