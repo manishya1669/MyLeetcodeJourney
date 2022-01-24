c3a7215306b55b413916f002c55c0180e9f933fc
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-01-24 07:40:07 +0530

    Time: 3 ms (39.46%), Space: 6.1 MB (19.00%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+         int c=0;
+        int l = 0;
+            int count =0;
+        if(isupper(word[0]) ){
+           
+                
+            for(int i =1;i<n;i++){
+                if(isupper(word[i])){
+                    count++;
+                }
+              else if(islower(word[i])){
+                  c++;
+              }
+            }
+           
+        }
+      else  if(islower(word[0])){
+          for(int i =1;i<n;i++){
+              if(islower(word[i]))
+                  l++;
+          }
+      }
+        if( (count==n-1) || (c==n-1) || (l==n-1))
+            return true;
+        return false;
+    }
+};
