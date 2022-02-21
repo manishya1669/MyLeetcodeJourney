27eaae6275a678984bb34f46ae1d487b6c0b439d
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-02-21 09:04:20 +0530

    Time: 18 ms (80.59%), Space: 19.6 MB (88.96%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -13,15 +13,3 @@ public:
         return 0;
     }
 };
-//  int majorityElement(vector<int>& nums) {
-//         unordered_map<int, int> freq;
-//         for (int it : nums)
-//             freq[it]++;
-
-//         for(auto it : freq){
-//             if(it.second > nums.size()/2)
-//                 return it.first;
-//         }
-
-//         return 0;
-//     }
