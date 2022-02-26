45178662738d7396d7d907c0dc0003a9d942e3e2
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-02-26 18:07:15 +0530

    Added solution - LeetHub

---
+++ 11. Reversing the columns of a Matrix - GFG/11.-reversing-the-columns-of-a-matrix.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to reverse the columns of a matrix.
+    void reverseCol(vector<vector<int> > &matrix)
+    {
+       int n = matrix.size();
+       int m = matrix[0].size();
+       
+     //  if(n==0 || m ==0)return 0;
+       for(int i=0;i<n;i++){
+           int low =0;
+           int high =m-1;
+           while(low<high){
+               swap(matrix[i][low],matrix[i][high]);
+               low++;
+               high--;
+           }
+       }
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.reverseCol(matrix);
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
