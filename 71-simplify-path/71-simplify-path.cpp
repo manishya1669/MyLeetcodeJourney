55d9e98d767794023677fba5081f6912d332e068
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-03-14 16:06:38 +0530

    Time: 8 ms (62.74%), Space: 10.6 MB (25.77%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -4,43 +4,42 @@ public:
         int n=path.length();
         stack<string>s;
                 
-        if(path[n-1]!='/') // so that initially path always ends with '/'
+        if(path[n-1]!='/') 
             path+="/", ++n;
             
-        int i=1; // since path always starts from '/'
+        int i=1;
         string ans="";
         string temp="";
         while(i<n){
             
-            if(path[i]=='/'){ // check only if we encounter '/'
+            if(path[i]=='/'){ 
                 
                 if(temp=="" || temp=="."){
-                    // ignore
+                  
                 }
                 else if(temp==".."){
-                    if(!s.empty()) s.pop(); // pop the top element from stack if exists
+                    if(!s.empty()) s.pop(); 
                 }
                 else{
-                    s.push(temp); //push the directory or file name to stack
+                    s.push(temp); 
                 }
                 
-                temp=""; // reset temp
+                temp=""; 
             }
             else{
-                temp.push_back(path[i]); // else append to temp
+                temp.push_back(path[i]);
             }
             
-            ++i; // increment index
+            ++i; 
         }
         
-        while(!s.empty()){ // add all the stack elements
+        while(!s.empty()){ 
             ans="/"+s.top()+ans;
             s.pop();
         }
         
-        if(ans.length()==0) // if no directory or file is present
-            ans="/"; // minimum root directory must be present in ans
-        
+        if(ans.length()==0) 
+            ans="/"; 
         return ans;
     }
 };
