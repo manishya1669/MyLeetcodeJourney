f2465e370809403edae33c796045dc88080eb7f3
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-03-24 15:16:45 +0530

    Added solution - LeetHub

--- 22. Case-specific Sorting of Strings - GFG/22.-casespecific-sorting-of-strings.cpp
+++ 22. Case-specific Sorting of Strings - GFG/22.-casespecific-sorting-of-strings.cpp
@@ -12,6 +12,64 @@ class Solution
     //Function to perform case-specific sorting of strings.
     string caseSort(string str, int n)
     {
+        
+        // string upper;
+        // string lower;
+        
+        // for(int i =0;i<n;i++){
+        //     if(isupper(str[i]))
+        //          {upper.push_back(str[i]);}
+            
+        //     else
+        //       { lower.push_back(str[i]);}
+        // }
+        
+        // sort(upper.begin(),upper.end());
+        // sort(lower.begin(),lower.end());
+        
+        // int i=0;
+        // int j =0;
+        // string res;
+        // for(int i=0;i<n;i++){
+        //     if(isupper(str[i]))
+        //       {res.push_back(upper[i]);
+        //       i++;}
+               
+        //     else
+        //       {res.push_back(lower[j]);
+        //       j++;}
+        // }
+        
+        // return res;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         // your code here
         string upper;
         string lower;
