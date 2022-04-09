3eab585ecef5ec43d1da0575046f178b4fa2bc3d
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-04-09 21:44:13 +0530

    Time: 24 ms (42.68%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    struct node {
-        int no;
-        int fre;
-        node(int a,int b){
-            no = a;
-            fre = b;
-        }
-    
-    };
-    struct compare{
-        bool operator()(node const &a , node const &b){
-            return a.fre<b.fre;
-        }
-    };
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int ,int>umap;
-        for(auto x:nums){
-            umap[x]++;
-        }
-        priority_queue<node,vector<node>,compare>heap;
-        for(auto it:umap){
-            heap.push(node(it.first,it.second));
-        }
-        vector<int>ans;
-        while(k--){
-            node temp = heap.top();
-            heap.pop();
-            ans.push_back(temp.no);
-        }
-        return ans;
-    }
+      vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+          unordered_map<int,int>umap;
+          for(auto x:nums){
+              umap[x]++;
+          }
+       
+          vector<pair<int,int>>v;
+          for(auto x:umap){
+              v.push_back(make_pair(x.second,x.first));
+          }
+          sort(v.begin(),v.end(),greater<>());
+          vector<int>ans;
+          for(int i=0;i<k;i++){
+              ans.push_back(v[i].second);
+          }
+          return ans;
+      }
 };
