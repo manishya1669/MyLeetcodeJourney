73497b4d1ece217dd31060cc5054b97d10421b50
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-04-16 07:49:05 +0530

    Time: 4 ms (25.49%), Space: 6.2 MB (35.58%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        string s=to_string(num); //first covert the num into a string for easy traversal
-        priority_queue<int> odd, even; // then take 2 priority queue odd & even
+        string s=to_string(num);
+        priority_queue<int> odd, even;
         for(auto x: s){
-            int tmp=x-'0'; // covert char to int
-            if(tmp%2==0) even.push(tmp); // if tmp is even the push it into even priority queue
-            else odd.push(tmp); // else tmp is odd &  the push it into odd priority queue
+            int tmp=x-'0';
+            if(tmp%2==0) even.push(tmp); 
+            else odd.push(tmp); 
         }
-		// now traverse the string and find whether it is a odd no.'s position or even no.'s position
-        for(auto& x: s){
-            int tmp=x-'0'; // converting char to int
-            if(tmp%2==0) x= even.top()+'0', even.pop(); // if it is even no.'s position then put there, even priority queue's top element & pop that element
-            else x= odd.top()+'0', odd.pop();  // else it is odd no.'s position so put there, odd priority queue's top element & pop that element
+
+        
+       for(auto& x: s){
+            int tmp=x-'0'; 
+            if(tmp%2==0) x= even.top()+'0', even.pop(); 
+            else x= odd.top()+'0', odd.pop(); 
         }
-        return stoi(s); // finally convert the string into int and return it!
+        return stoi(s);
     }
 };
