9de38c0075a8e136e2245c25070ce4f9566705b3
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-06-14 09:00:33 +0530

    Time: 88 ms (14.13%), Space: 7.3 MB (91.90%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int LCS(string x,string y, int n,int m,int dp[501][501]){
+    int LCS(string &x,string &y, int n,int m,int dp[501][501]){
         
-        if(m==0 || n==0)
-            return  0;
-        
-        if(dp[n][m] !=-1)
-            return dp[n][m];
-        
-        if(x[n-1] == y[m-1]){
-             return dp[n][m]=  1 + LCS(x,y,n-1,m-1,dp);
+       //Initialization
+        for(int i =0;i<=n;i++){
+            for(int j = 0;j<=m;j++){
+                if(i ==0 || j==0)
+                    dp[i][j]=0;
+            }
         }
-        else{
-            return dp[n][m]= max(LCS(x,y,n-1,m,dp) , LCS(x,y,n,m-1,dp));
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(x[i-1] == y[j-1])
+                    {dp[i][j] = 1 + dp[i-1][j-1];}
+                
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
+        return dp[n][m];
     }
     
     
