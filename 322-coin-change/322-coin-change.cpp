4f275d085669641816b4e5fc19b410ddfc110688
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-06-24 17:30:14 +0530

    Time: 106 ms (67.68%), Space: 10.6 MB (79.43%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -8,31 +8,23 @@ public:
         //Initialization
         for(int i=0;i<=n;i++){
             for(int j =0;j<=amount;j++){
-                if(i==0)
-                    dp[i][j]=INT_MAX -1;
+                 if(i==0)
+                    dp[i][j]=1e5;
                 
-                 if(j==0)
+                 else if(j==0)
                      dp[i][j]=0;
                 
-                 if(i==1){
-                     if(j % coins[i-1]==0){
-                         dp[i][j]=j/coins[i-1];
-                     }else{
-                         dp[i][j]=INT_MAX-1;
-                     }
-                 }
-            }
-            for(int i =1;i<=n;i++){
-                for(int j =1;j<=amount;j++){
-                    if(coins[i-1]<=j){
+                 else  if(coins[i-1]<=j)
                         dp[i][j]= min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
-                    }else{
+                 else{
                         dp[i][j]=dp[i-1][j];
                     }
-                }
+               
+                 }
             }
-        } 
+            
+         
                     return dp[n][amount] > 1e4?-1:dp[n][amount];
-
     }
+    
 };
