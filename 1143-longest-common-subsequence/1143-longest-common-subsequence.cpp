ed29e9f952c1207d92696120939d414f576b8078
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-06-25 13:42:23 +0530

    Time: 23 ms (71.56%), Space: 10.6 MB (78.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-   
- 
     int longestCommonSubsequence(string text1, string text2) {
+        
         int n = text1.length();
         int m = text2.length();
-        int dp[1001][1001];
-            memset(dp,0,sizeof(dp));
-
-        return lcs(text1,text2,n,m,dp);
+        
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0){
+                    dp[i][j]=0;
+                    
+                }
+                if(j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                else{
+                  dp[i][j]= max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+      }
+        return dp[n][m];
     }
-      int lcs(string &t1,string &t2, int n, int m ,int dp[1001][1001]){
-       if(n==0 || m==0){
-           return 0;
-       }
-       //Memoization
-       if(dp[n][m] != 0)
-           return dp[n][m];
-       
-       if(t1[n-1] == t2[m-1]){
-           return dp[n][m] = 1 + lcs(t1,t2,n-1,m-1,dp);
-       }
-       else{
-           return dp[n][m]= max(lcs(t1,t2,n-1,m,dp),lcs(t1,t2,n,m-1,dp));
-       }
-           
-   }
 };
