5d25282318b01a79fbf16b3155ec79e0856aa718
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-06-27 20:40:46 +0530

    Time: 476 ms (8.83%), Space: 163.2 MB (13.03%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> ans;
-        f(s, 0, ans, res);
-        return res;
+    vector<vector<string>>v;
+    
+    bool ispalindrome(string s){
+        int i =0,j=s.length()-1;
+        
+        while(i<j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
     
-    void f(string s,int ind,vector<string> &ans, vector<vector<string>> &res){
-        if(ind==s.size()){
-            res.push_back(ans);
+    void result(string s, vector<string>ans){
+        if(s.length()==0){
+            v.push_back(ans);
             return;
         }
-        for(int i=ind; i<s.size(); i++){
-            if(isPalindrome(s, ind, i)){
-                ans.push_back(s.substr(ind, i-ind+1));
-                f(s, i+1, ans, res);
+        
+        for(int i =0;i<s.length();i++){
+            string temp = s.substr(0,i+1);
+            string last = s.substr(i+1);
+            if(ispalindrome(temp)){
+                ans.push_back(temp);
+                result(last,ans);
                 ans.pop_back();
             }
         }
     }
-    bool isPalindrome(string s, int start, int end)  {
-        while(start<=end){
-            if(s[start++]!=s[end--]){
-                return false;
-            }
-        }
-        return true;
-    } 
-    
-    
+    vector<vector<string>> partition(string s) {
+        vector<string>vec;
+        result(s,vec);
+        return v;
+        
+    }
 };
