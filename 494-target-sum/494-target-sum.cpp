be0fc159a03a5b0147dd1a882baf6c522c76b59b
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-06-28 20:20:03 +0530

    Time: 7 ms (96.32%), Space: 9.1 MB (60.90%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
-     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++)
-            sum+=nums[i];
-         //Calculating sum of elements of array
+    int findTargetSumWays(vector<int>& nums, int target) {
          
-         
+        int sum =0;
+        for(int i =0;i<nums.size();i++){
+            sum += nums[i];
+        }
         if(sum < abs(target) || (target+sum) %2 != 0) return 0;
-         
-         //Checking boundary conditions for passing test cases
-         
-        int s1=(target+sum)/2;
-        int t[n+1][s1+1];
-		//Initialization
-        for(int i=0;i<n+1;i++){ 
-            for(int j=0;j<s1+1;j++){
-                 if(i==0)
-                    t[i][j]=0;
-                if(j==0)
-                    t[i][j]=1;
+        int  s1 = ( sum + target)/2;
+        // Why to do this " sum + target"/ 2 So this is because to 
+        // solve it like a differnce fo subset problem
+        // where you have to divide a array into subset whose differce is given 
+        // i.e
+        //
+        //  positibe sum = s1
+        //  negative sum = s2;
+        // s1 -s2        = target     i.e ( 1+ 1+ 1+ 1+) - (1)
+        // similarlt
+        // s1 + s2       = total Sum;
+        // USing both equations,
+        // 2s1 = target + total sum;
+        // s1 = (target + totalSum)/2;
+        // Now you have to find no. of ways to get sum s1;
+        
+        // Uing subset problem
+        int n= nums.size();
+        int dp[n+1][s1+1];
+        
+        for(int i =0;i<=n;i++){
+            for(int  j =0;j<=s1;j++){
+                if(i==0)dp[i][j]=0;
+                
+                if(j==0)dp[i][j]=1;   
             }
         }
-         
-		//Dynamic Programming
-         
-        for(int i=1;i<n+1;i++)
-            for(int j=0;j<s1+1;j++){
-               
-                if(nums[i-1]<=j)
-                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
-                else
-                    t[i][j]=t[i-1][j];
+         for(int i =1;i<=n;i++){
+            for(int  j =0;j<=s1;j++){
+               if(nums[i-1]<=j)
+                   dp[i][j]= dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                
+                else{
+                dp[i][j]= dp[i-1][j];
             }
-        return t[n][s1]; 
-         
-         //Returning the last element of filled matrix through DP
-        
+            }
+             
+        }
+       return dp[n][s1];
     }
 };
