02ed152c76e26ca2bb11daf7299e1dc307384c5b
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-07-10 14:44:16 +0530

    Time: 122 ms (26.91%), Space: 16 MB (50.27%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    static bool comparator(vector<int>&a,vector<int>&b){
+    static bool com(vector<int> &a, vector<int>&b){
         return a[1]>b[1];
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        
-        sort(boxTypes.begin(),boxTypes.end(),comparator);
+      
+        sort(boxTypes.begin(),boxTypes.end(),com);
+        int n= boxTypes.size();
         int sum=0;
-        
-        for(auto box : boxTypes){
-            
-            if(truckSize< box[0]){
-                sum += truckSize *  box[1];
+        for(int  i=0;i<n;i++){
+            if(truckSize >=boxTypes[i][0]){
+                truckSize -=boxTypes[i][0];
+                sum +=boxTypes[i][1] * boxTypes[i][0];
+                cout<<sum<<" ";
+            }
+            else{
+                sum +=truckSize * boxTypes[i][1];
                 break;
             }
-            sum += box[0] * box[1];
-            truckSize -=box[0];
         }
-        
-        
-//         for(int i =0;i<boxTypes.size();i++){
-           
-//             if(truckSize>boxTypes[i][0]){
-//                 sum+=boxTypes[i][0] * boxTypes[i][1];
-//                 truckSize= truckSize - boxTypes[i][0];
-                
-//             }else{
-//                 sum += truckSize * boxTypes[i][1];
-                
-//             }
-            
-        
         return sum;
+        
     }
 };
