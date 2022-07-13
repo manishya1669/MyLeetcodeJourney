1feac08a1716339e424cae358a99e8b1ad9e0bbd
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-07-13 17:50:15 +0530

    Time: 228 ms (60.34%), Space: 36.4 MB (79.37%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -6,18 +6,19 @@ public:
     
         if(n <= 1)  return n;
         for(int i = 0; i < n; i++) p.push(nums[i]); 
-        int current = p.top(), longest = 0, count = 0;
-        p.pop();
+       // int current = p.top(),
+        int longest = 0, count = 0;
+       // p.pop();
         while(!p.empty()){
             int item = p.top();
             p.pop();
-            if(item == current - 1)  count++;
-            else if(item == current) continue;
+            if(item == p.top() + 1)  count++;
+            else if(item == p.top()) continue;
             else {
                 if(count > longest)  longest = count;
                 count = 0;
             }
-            current = item;
+           // current = item;
         }
         if(count > longest) longest = count;
         return longest + 1;
