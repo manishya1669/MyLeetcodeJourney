f4eabbc61abc5b237ad09437ff4fab0f680632c4
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-07-13 18:48:04 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,36 +10,41 @@ using namespace std;
 
 class Solution{
   public:
-    int longestPalinSubseq(string A) {
-        int n = A.length();
-        string s = A;
-        reverse(s.begin(),s.end());
-        
-        int dp[1001][1001];
-          for(int i=0;i<n+1;i++)
-       {
-           for(int j=0;j<n+1;j++)
-           {
-               if(i==0 ||j==0)
-               {
-                   dp[i][j]=0;
-               }
+   int LCS(string s1, string s2 ,int n ){
+       
+       int dp[n+1][n+1];
+       
+       for(int i =0;i<=n;i++){
+           for(int j =0;j<=n;j++){
                
+               if(i==0  || j==0)
+                   dp[i][j]=0;
            }
        }
-        for(int i=1;i<=n;i++){
-            for(int j =1;j<=n;j++){
-                       
-                 if(A[i-1]==s[j-1]){
-                    dp[i][j]= 1 + dp[i-1][j-1];
-                }
-                else
-                
-               dp[i][j]= max(dp[i-1][j],dp[i][j-1]);   
-                
-            }
-        }
-        return dp[n][n];
+       for(int i =1;i<=n;i++){
+           for(int j =1;j<=n;j++){
+               if(s1[i-1]==s2[j-1]){
+                   dp[i][j]= 1 + dp[i-1][j-1];
+               }
+               else{
+                   dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+               }
+           }
+       }
+      // cout<<dp[n][n];
+       return dp[n][n];
+       
+       
+       
+   }
+    int longestPalinSubseq(string A) {
+        string B= A;
+        reverse(B.begin(),B.end());
+        int n = A.length();
+        
+        int ans = LCS(A,B,n);
+        //cout<<ans<<" ";
+        return ans;
     }
 };
 
