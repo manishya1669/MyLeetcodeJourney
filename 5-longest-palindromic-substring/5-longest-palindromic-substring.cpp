cb492d1d14e1be302b1a424d009a24b6020dece4
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-07-16 17:36:53 +0530

    Time: 1544 ms (5.01%), Space: 246.2 MB (15.88%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-       string longestPalindrome(string s)
-    {
-        int n = s.size();
-        if (n == 0)
-            return "";
-
-        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
-        bool dp[n][n];
-
-        //Initialize with false
-
-        memset(dp, 0, sizeof(dp));
-
-        //Every Single character is palindrome
-        for (int i = 0; i < n; i++)
-            dp[i][i] = true;
-
-        string ans = "";
-        ans += s[0];
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (s[i] == s[j])
-                {
-                    //If it is of two character OR if its susbtring is palindrome.
-                    if (j - i == 1 || dp[i + 1][j - 1])
-                    {
-                        //Then it will also a palindrome substring
-                        dp[i][j] = true;
-
-                        //Check for Longest Palindrome substring
-                        if (ans.size() < j - i + 1)
-                            ans = s.substr(i, j - i + 1);
-                    }
+     string longestPalindrome(string s) {
+        string s1=s;
+        reverse(s.begin(),s.end());
+        string ans;
+       int n=s.size();
+        int res=0;
+      int dp[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0)
+                    dp[i][j]=0;
+               else if(s[i-1]==s1[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                   
+                   
                 }
+                else
+                    dp[i][j]=0;
+                
+                 if(dp[i][j]>res)
+                    {
+                        string temp=s.substr(i-dp[i][j],dp[i][j]);
+                        string rev=temp;
+                        reverse(rev.begin(),rev.end());
+                        if(temp==rev){    //check if common substring is palindromic or not
+                             ans=temp;
+                        res=dp[i][j];
+                        }
+                           
+                    }
             }
+            
         }
         return ans;
-    
-    }
+     }
 };
