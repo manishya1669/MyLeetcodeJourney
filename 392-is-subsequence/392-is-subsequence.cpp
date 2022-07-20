a290708468d25434dbb1c1ce41e6f3cbf5dca7da
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-07-20 07:52:22 +0530

    Time: 4 ms (40.07%), Space: 6.9 MB (13.36%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int index =0;
-        int n = s.length();
-        for(int i =0;i<t.length();i++){
-            if(s[index]==t[i]){
-                index++;
+    int LCS(string s1, string s2){
+        int n= s1.length();
+        int m = s2.length();
+        int dp[n+1][m+1];
+        
+        for(int  i=0;i<=n;i++){
+            for(int j =0;j<=m;j++){
+                if(i==0 || j==0)
+                    dp[i][j]= 0;
             }
         }
-        return n==index?true:false;
+        
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s1[i-1]==s2[j-1])
+                   { dp[i][j]= 1 + dp[i-1][j-1];
+                    cout<<dp[i][j]<<" ";}
+                
+                else
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][m];
+        
+        
+    }
+    bool isSubsequence(string s, string t) {
+        
+        int mini = min(s.length(),t.length());
+        if(s.length()==0  )return true;
+        if(t.length()==0)  return false;
+        int x = LCS(s,t);
+        return x==mini?true:false;
+       
     }
 };
