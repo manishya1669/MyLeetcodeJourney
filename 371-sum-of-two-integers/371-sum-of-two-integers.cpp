fbd11b0d311d9575784a90612589d02e5608e6b7
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-08-13 17:11:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.53%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+      //   int count =0;
+      // if(a>=0 && b>=0){for(int i =0;i<a;i++){
+      //       count++;
+      //   }
+      //   for(int i=0;i<b;i++){
+      //       count++;
+      //   }}
+      //   return count;
+        return (a+b);
+    }
+};
