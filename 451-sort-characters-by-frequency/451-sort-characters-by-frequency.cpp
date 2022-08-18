e96f7b1cbb26d234f5d27cf18a1c74c91546f185
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2022-08-18 16:33:32 +0530

    Time: 18 ms (54.55%), Space: 8.4 MB (76.86%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    static bool comp(pair<int ,char>&a,pair<int,char>&b){
+        return a.first>b.first;
+    }
     string frequencySort(string s) {
-        
         unordered_map<char,int>umap;
         for(auto x:s){
             umap[x]++;
         }
         vector<pair<int,char>>v;
-        
         for(auto x:umap){
-            v.push_back(make_pair(x.second,x.first));
+            v.push_back({x.second,x.first});
+            cout<<x.second<<x.first<<endl;
         }
-        sort(v.begin(),v.end(),greater<>());
-        string str = "";
-        for(int i =0;i<v.size();i++){
-           int x = v[i].first;
-            while(x--){
-                 str+=v[i].second;
+        sort(v.begin(),v.end(),comp);
+        string str="";
+        for(auto x:v){
+            int count=x.first;
+            while(count--){
+                 str+=x.second;
             }
         }
         return str;
+        
     }
 };
