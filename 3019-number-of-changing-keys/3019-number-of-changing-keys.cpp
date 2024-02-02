ea22e6dfad51e49629b4d7f611e944ea2a6e6501
Manishyadav <85441182+manishya1669@users.noreply.github.com>
2024-02-02 09:23:25 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (91.52%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        
+        int count =0;
+     
+        for(int i =1;i<s.length();i++){
+            char c1=tolower(s[i-1]);
+            char c2 = tolower(s[i]);
+            if(c1 != c2){
+                count++;
+            }
+            
+        }
+        return count;
+    }
+};
